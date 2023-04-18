195b640cd583500f33bc82d5b2553a7e62a6a30b
RockabyeW <112534247+RockabyeW@users.noreply.github.com>
2023-04-18 15:48:05 -0400

    Add files via upload

---
+++ xw642-hw1-main/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <img alt="test image" :src="`${baseDir}images/test.png`">
+  </div>
+</template>
+
+<style>
+img {
+  display: block;
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+  }
+}
+</style>
